b1b3ec588b02482b4d660ac08ae3e68088deddf9
Giorgi Khitarishvili <giorgi.khitarishvili@outlook.com>
2024-10-11 22:23:06 +0400

    second commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# dct1 = {
+#     "David": ["Programing", "Calculus", "Algorithms"],
+#     "John": ["Calculus", "OOP", "Data Bases"],
+#     "George": ["Web development", "Data Bases", "Calculus", "Algorithms"],
+#     "Steve": "Linux"
+# }
+
+
+
+
+# for key, value in dct1.items():
+#     # print(value)
+#     val = value.split()
+#     print(val)
+#     for subj in value:
+#         print(subj, key)
+
+
+
--- view.py
+++ view.py
@@ -1,10 +1,14 @@
 import sys
 
 import pygame
+from pygame.examples.video import backgrounds
 
 from controller import Player
 
 pygame.init()
+
+
+background_image = pygame.image.load("tortoise.png")
 
 
 WIDTH = 800
@@ -20,6 +24,11 @@ clock = pygame.time.Clock()
 
 player_1 = Player()
 
+pygame.display.set_caption("user: ")
+
+font = pygame.font.SysFont('Arial', 30)
+username = ""
+
 running = True
 
 while running:
@@ -27,7 +36,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    pygame.draw.rect(screen, BLACK, (player_1.x, player_1.y, player_1.size, player_1.size))
+        if event.type == pygame.K_DOWN:
+            username += event.unicode
+    # pygame.draw.rect(screen, BLACk, (player_1.x, player_1.y, player_1.size, player_1.size))
+    screen.blit(pygame.transform.scale(background_image, (player_1.size, player_1.size)), (player_1.x, player_1.y))
     keyboard = pygame.key.get_pressed()
     # print(keyboard)
     dx = 0
@@ -40,9 +52,16 @@ while running:
         dy -= 1
     if keyboard[pygame.K_DOWN]:
         dy += 1
+
+    username_screen = font.render(username, True, RED)
+    screen.blit(username_screen, (player_1.x, player_1.y + 50))
+
+
     player_1.move_player(dx, dy)
     pygame.display.update()
     clock.tick(30)
+
+print(username)
 
 pygame.quit()
 sys.exit()
