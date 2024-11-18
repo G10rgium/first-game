062577e01f8e1c4bdc7107cfb5801fa163ea7013
Giorgi Khitarishvili <giorgi.khitarishvili@outlook.com>
2024-11-18 16:55:20 +0400

    almost finished_1

--- main.py
+++ main.py
@@ -1,41 +1,9 @@
 import sys
-from idlelib.replace import replace
-from os import remove
 
 import pygame
 
 from random import randint
 
-# class Player:
-#     def __init__(self):
-#         self.size = 80
-#         self.x = 960
-#         self.y = 540
-#         self.speed = 5
-#
-#     def move_player(self, x_position, y_position):
-#         # dx = x_position - self.x
-#         # dy = y_position - self.y
-#         self.x += x_position * self.speed
-#         self.y += y_position * self.speed
-#
-#
-# class Bamboo:
-#     def __init__(self):
-#         self.size = 50
-#         self.x = randint(0, 1920)
-#         self.y = randint(0, 1080)
-#
-#
-# class Player2:
-#     def __init__(self):
-#         self.size = 80
-#         self.x = 560
-#         self.y = 240
-#         self.speed = 5
-#     def move_player(self, x_position, y_position):
-#         self.x += x_position * self.speed
-#         self.y += y_position * self.speed
 
 pygame.init()
 
@@ -91,31 +59,11 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-
-
-
-
-        # if event.type == pygame.KEYDOWN:
-        #     username += event.unicode
-    # pygame.draw.rect(screen, BLACk, (player_1.x, player_1.y, player_1.size, player_1.size))
     screen.blit(size_1, player_1)
     screen.blit(size_3, bambo)
     screen.blit(size_2, hunter_1)
 
-    # print(keyboard)
-    # dx = 0
-    # dy = 0
-    # wx = 0
-    # wy = 0
-    # def update():
-    #     if keyboard[pygame.K_LEFT] and player_1.x > 0:
-    #         player_1.x -= 1
-    #     elif keyboard[pygame.K_RIGHT] and player_1.x < WIDTH:
-    #         player_1.x += 1
-    #     elif keyboard[pygame.K_UP] and player_1.y > 0:
-    #         player_1.y -= 1
-    #     elif keyboard[pygame.K_DOWN] and player_1.y < HEIGHT:
-    #         player_1.y += 1
+
     keyboard = pygame.key.get_pressed()
     if keyboard[pygame.K_LEFT]:
         player_1.x -= vel_panda
@@ -136,7 +84,6 @@ while running:
 
     username_screen = font.render("SCORE: " + str(score), False, RED)
     screen.blit(username_screen, (960, 0))
-    # update()
 
     if player_1.colliderect(hunter_1):
         pygame.draw.rect(screen, (255, 0, 0), player_1, 4)
@@ -158,7 +105,6 @@ while running:
     pygame.display.update()
     clock.tick(30)
 
-# print(username)
 pygame.quit()
 sys.exit()
 
